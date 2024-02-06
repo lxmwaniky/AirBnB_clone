efc47008a4a4b5f6e8717cbcd32fe79a906f0a40
marky376 <mmutuamark@gmail.com>
2024-02-06 04:53:06 +0300

    Added the child classes in console.py

--- console.py
+++ console.py
@@ -5,3 +5,21 @@ from datetime import datetime
 
 class BaseModel:
     pass
+
+class Amenity(BaseModel):
+    pass
+
+class City(BaseModel):
+    pass
+
+class Place(BaseModel):
+    pass
+
+class Review(BaseModel):
+    pass
+
+class State(BaseModel):
+    pass
+
+class User(BaseModel):
+    pass
