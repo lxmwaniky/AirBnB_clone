e0911d255cfddcf93889d697b6867c45a3a32cc4
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-06 21:39:18 +0300

    Imported classes from models folder

--- console.py
+++ console.py
@@ -3,6 +3,14 @@
 import cmd
 from datetime import datetime
 
+# Imported classes from models folder
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 CLASSES = {
     "BaseModel": BaseModel,
     "Amenity": Amenity,
