e688c87deb6af93f1c4d1d734cf2ce55730b220e
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-06 21:25:40 +0300

    Simplified Classes in a Dictionary

--- console.py
+++ console.py
@@ -3,23 +3,12 @@
 import cmd
 from datetime import datetime
 
-class BaseModel:
-    pass
-
-class Amenity(BaseModel):
-    pass
-
-class City(BaseModel):
-    pass
-
-class Place(BaseModel):
-    pass
-
-class Review(BaseModel):
-    pass
-
-class State(BaseModel):
-    pass
-
-class User(BaseModel):
-    pass
+CLASSES = {
+    "BaseModel": BaseModel,
+    "Amenity": Amenity,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User
+}
