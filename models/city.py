2c3d9e3ed77e5e9f0aabae68329ba5ebdbf1eafe
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-07 23:25:59 +0300

    Add City class to models/city.py

--- models/city.py
+++ models/city.py
@@ -3,3 +3,10 @@
 This module defines the City class, which represents a city in the HBNB project.
 """
 from models.base_model import BaseModel
+
+class City(BaseModel):
+        """
+        This class defines the City object, which represents a city in the HBNB project.
+        """
+        state_id = ""
+        name = ""
