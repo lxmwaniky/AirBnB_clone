25a90abd1bf62ffbf0ed1e043208c0cca2f85593
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-07 23:27:32 +0300

    Add Review class to models/review.py

--- models/review.py
+++ models/review.py
@@ -3,3 +3,11 @@
 This module defines the Review class, which represents a review in the HBNB project.
 """
 from models.base_model import BaseModel
+
+class Review(BaseModel):
+        """
+        This class defines the Review object, which represents a review in the HBNB project.
+        """
+        place_id = ""
+        user_id = ""
+        text = ""
