c7729b35b581ee629a4f2291c2cf2b3ee51525c2
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-07 23:24:25 +0300

    Add Amenity class to models/amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,3 +3,9 @@
 This module defines the Amenity class, which represents an amenity in the HBNB project.
 """
 from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+        """
+        This class defines the Amenity object, which represents an amenity in the HBNB project.
+        """
+        name = ""
