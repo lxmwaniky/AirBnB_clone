e7ac12f959eaa0364c72592a39598276cc5011c2
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-07 23:25:19 +0300

    Add State class to models

--- models/state.py
+++ models/state.py
@@ -3,3 +3,7 @@
 This module defines the State class, which represents a state in the HBNB project.
 """
 from models.base_model import BaseModel
+
+class State(BaseModel):
+        
+        name = ""
