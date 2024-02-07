ba3370ba5e0a9247bf9b7b1ebebc7df3c8ae4794
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-07 23:17:07 +0300

    Add command-line functionality to interact with models

--- console.py
+++ console.py
@@ -8,13 +8,69 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from models.base_model import BaseModel
+from models import storage
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+    classes = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]
 
-CLASSES = {
-    "BaseModel": BaseModel,
-    "Amenity": Amenity,
-    "City": City,
-    "Place": Place,
-    "Review": Review,
-    "State": State,
-    "User": User
-}
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Called when an empty line is entered in response to the prompt"""
+        pass
+
+    def do_create(self, line):
+        """Create a new instance of BaseModel, save it to a JSON file, and print the id"""
+        if not line:
+            print("** class name missing **")
+        elif line not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(line)()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an instance based on the class name and id"""
+        args = line.split()
+        if not line:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            storage = eval(args[0] + ".all()")
+            if key in storage:
+                print(storage[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        args = line.split()
+        if not line:
+            print("** class name missing **")
+        elif args[0] not in self.classes:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            storage = eval(args[0] + ".all()")
+            if key in storage:
+                del storage[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
