68624564cb28029b43ac3b3eb573c63713ecb0d5
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-07 23:23:34 +0300

    Add User model with attributes

--- models/user.py
+++ models/user.py
@@ -3,3 +3,11 @@
 This module defines the User class, which represents a user in the HBNB project.
 """
 from models.base_model import BaseModel
+
+class User(BaseModel):
+    """Defines the User model."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
