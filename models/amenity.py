24fb7f8d6fb14e792f75bd29bbb0dd903b36ae1d
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-08 00:02:38 +0300

    Refactor Amenity class documentation

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-This module defines the Amenity class, which represents an amenity in the HBNB project.
+This module defines the Amenity class,
+which represents an amenity in the HBNB project.
 """
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
-        """
-        This class defines the Amenity object, which represents an amenity in the HBNB project.
-        """
-        name = ""
+    """
+    This class defines the Amenity object,
+    which represents an amenity in the HBNB project.
+    """
+    name = ""
