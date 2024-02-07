69f10bc50d82968bd2d90fcd0950721b99969826
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-07 23:19:59 +0300

    Add Amenity and City classes

---
+++ models/amenity.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""
+This module defines the Amenity class, which represents an amenity in the HBNB project.
+"""
+from models.base_model import BaseModel
---
+++ models/city.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""
+This module defines the City class, which represents a city in the HBNB project.
+"""
+from models.base_model import BaseModel
