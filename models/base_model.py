db8153e800615865e4f132c36f3ad867629a00c2
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-08 21:41:42 +0300

    Add TIME_FORMAT constant and update __init__ method in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -12,4 +12,12 @@ class BaseModel:
     This class defines the BaseModel object,
     which serves as the base class for all other classes
     """
+    TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new BaseModel object
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                
