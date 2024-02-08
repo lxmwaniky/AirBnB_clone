28a6c39cecb72ffba3ec2e66aed9bd3e0f581263
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-08 21:37:10 +0300

    Add BaseModel class as base class for all other classes

--- models/base_model.py
+++ models/base_model.py
@@ -6,3 +6,10 @@ other classes in this project.
 from uuid import uuid.uuid4
 from datetime import datetime
 import models
+
+class BaseModel:
+    """
+    This class defines the BaseModel object,
+    which serves as the base class for all other classes
+    """
+    def __init__(self, *args, **kwargs):
