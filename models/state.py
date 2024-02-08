d63e2efec4aa7c7eea285b1dc51b9b9a81901735
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-08 23:03:42 +0300

    Removed Models

--- models/state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-This module defines the State class, which represents a state in the HBNB project.
-"""
-from models.base_model import BaseModel
-
-class State(BaseModel):
-        
-        name = ""
