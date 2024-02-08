36b3f1082ecfa1c53eaaf484f4530c504fe8dd1b
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-08 22:46:45 +0300

    Refactor BaseModel class and add __str__, save, and to_dict methods

--- models/base_model.py
+++ models/base_model.py
@@ -8,25 +8,28 @@ from datetime import datetime
 import models
 
 class BaseModel:
-    """
-    This class defines the BaseModel object,
-    which serves as the base class for all other classes
-    """
-    DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
-
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new BaseModel object
-        """
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, self.DATE_FORMAT)
-                if key != "__class__":
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
-        
+    
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.updated_at = datetime.now()
+        models.storage.save()
+    
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
