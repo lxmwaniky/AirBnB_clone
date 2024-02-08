57ece99ed22a3ee19fb0ebb4271b39c5933559df
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-08 23:11:51 +0300

    Minor changes

--- console.py
+++ console.py
@@ -2,10 +2,3 @@
 
 import cmd
 from datetime import datetime
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.base_model import BaseModel
