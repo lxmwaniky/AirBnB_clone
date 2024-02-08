5c8ea556426f942831072f60fd00cb1da06145e2
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-08 21:51:13 +0300

    Update date format in BaseModel constructor

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
     This class defines the BaseModel object,
     which serves as the base class for all other classes
     """
-    TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+    DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
     def __init__(self, *args, **kwargs):
         """
@@ -20,4 +20,13 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
-                
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, self.DATE_FORMAT)
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+        
