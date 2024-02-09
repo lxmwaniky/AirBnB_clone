539db9d60998541f8b7fd2fff005cbd433da3de9
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-09 15:26:52 +0300

    Add HBNBCONSOLE class to console.py

--- console.py
+++ console.py
@@ -2,3 +2,16 @@
 
 import cmd
 from datetime import datetime
+
+class HBNBCONSOLE(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        return True
+    def do_help(self, line):
+        return super().do_help(line)
+    def do_quit(self, line):
+        return True
+
+if __name__ == '__main__':
+    HBNBCONSOLE().cmdloop()
