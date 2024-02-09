b38374c4a08ce21f249cba61d0cbf4440ef611c6
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-09 15:49:47 +0300

    Refactor HBNBConsole class in console.py

--- console.py
+++ console.py
@@ -2,19 +2,23 @@
 
 import cmd
 from datetime import datetime
+from cmd import Cmd
 
-class HBNBConsole(cmd.Cmd):
+class HBNBConsole(Cmd):
     prompt = '(hbnb) '
 
     def do_EOF(self, line):
         return True
+
     def do_help(self, line):
-        return True
+        pass
+
     def do_quit(self, line):
         return True
+
     def emptyline(self):
         pass
     
 
 if __name__ == '__main__':
-    HBNBCONSOLE().cmdloop()
+    HBNBConsole().cmdloop()
