da79553021ae800202c0251ea5d787e8354ee70d
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-11 04:58:55 +0300

    Add City class to models

---
+++ tests/test_city.py
@@ -0,0 +1,22 @@
+import unittest
+from models.city import City
+
+class TestCity(unittest.TestCase):
+    """
+    A test case for the City class.
+    """
+
+    def setUp(self):
+        self.city = City()
+
+    def tearDown(self):
+        pass
+
+    def test_state_id_default_value(self):
+        self.assertEqual(self.city.state_id, "")
+
+    def test_name_default_value(self):
+        self.assertEqual(self.city.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
