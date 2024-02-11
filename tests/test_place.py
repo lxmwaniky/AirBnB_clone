a5eb88cf39bf8e2e21a74ef5fd179bbafdc71c92
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-11 05:02:32 +0300

    Update login form validation***

---
+++ tests/test_place.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import patch
+from models.place import Place
+
+class TestPlace(unittest.TestCase):
+    """
+    This class contains unit tests for the Place class.
+    """
+
+    def setUp(self):
+        self.place = Place()
+
+    def tearDown(self):
+        pass
+
+    def test_attributes(self):
+        self.assertEqual(self.place.city_id, "")
+        self.assertEqual(self.place.user_id, "")
+        self.assertEqual(self.place.name, "")
+        self.assertEqual(self.place.description, "")
+        self.assertEqual(self.place.number_rooms, 0)
+        self.assertEqual(self.place.number_bathrooms, 0)
+        self.assertEqual(self.place.max_guest, 0)
+        self.assertEqual(self.place.price_by_night, 0)
+        self.assertEqual(self.place.latitude, 0.0)
+        self.assertEqual(self.place.longitude, 0.0)
+        self.assertEqual(self.place.amenity_ids, [])
+
+if __name__ == '__main__':
+    unittest.main()
