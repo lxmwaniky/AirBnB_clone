7a624b3972bb83cafafc9d68caa451390a07b8b7
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-11 04:56:10 +0300

    Add Amenity class that inherits from BaseModel

---
+++ tests/test_amenity.py
@@ -0,0 +1,19 @@
+import unittest
+from models.amenity import Amenity
+
+class TestAmenity(unittest.TestCase):
+    def setUp(self):
+        self.amenity = Amenity()
+
+    def tearDown(self):
+        pass
+
+    def test_name_default_value(self):
+        self.assertEqual(self.amenity.name, "")
+
+    def test_name_assignment(self):
+        self.amenity.name = "Swimming Pool"
+        self.assertEqual(self.amenity.name, "Swimming Pool")
+
+if __name__ == '__main__':
+    unittest.main()
