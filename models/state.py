a82105075c67f5d54dad6ef64cb08fb304b3fec3
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-11 05:07:04 +0300

    Add State class to models

---
+++ models/state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""State Module:
+Inherits from Superclass BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class that represents a state in the AirBnB clone application.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+    name: str = ""
