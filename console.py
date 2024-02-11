db695c6e7b637a70d8caff76493de0d9d5ec248e
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-11 05:18:11 +0300

    Add import statements for models

--- console.py
+++ console.py
@@ -3,6 +3,14 @@
 import cmd
 import re
 from datetime import datetime
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
