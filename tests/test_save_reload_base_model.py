f9d0ee377887588fbca6fac72e357880c7bccb5d
marky376 <mmutuamark@gmail.com>
2024-02-13 06:03:24 +0300

    Sorting issues

--- tests/test_save_reload_base_model.py
+++ tests/test_save_reload_base_model.py
@@ -2,15 +2,19 @@
 from models import storage
 from models.base_model import BaseModel
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
+def main():
+    all_objs = storage.all()
+    print("-- Reloaded objects --")
+    for obj_id in all_objs.keys():
+        obj = all_objs[obj_id]
+        print(obj)
 
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
+    print("-- Create a new object --")
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    my_model.save()
+    print(my_model)
+
+if __name__ == "__main__":
+    main()
